630454c997da51da2f46991d07df6b8e5da86181
yeesoft <sto712@gmail.com>
2015-07-30 17:32:36 +0300

    added general settings migration

---
+++ migrations/m150730_172448_add_general_settings.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150730_172448_add_general_settings extends Migration
+{
+
+    public function up()
+    {
+
+        $this->insert('setting',
+            ['group' => 'general', 'key' => 'title', 'value' => 'Yee Site']);
+
+        $this->insert('setting',
+            ['group' => 'general', 'key' => 'description', 'value' => '']);
+
+        $this->insert('setting',
+            ['group' => 'general', 'key' => 'email', 'value' => '', 'description' => 'This address is used for admin purposes, like new user notification.']);
+
+        $this->insert('setting',
+            ['group' => 'general', 'key' => 'dateformat', 'value' => 'F j, Y']);
+
+        $this->insert('setting',
+            ['group' => 'general', 'key' => 'timeformat', 'value' => 'g:i a']);
+
+        $this->insert('setting',
+            ['group' => 'general', 'key' => 'timezone', 'value' => 'Europe/London']);
+    }
+
+    public function down()
+    {
+        $this->delete('setting', ['group' => 'group', 'key' => 'title']);
+        $this->delete('setting', ['group' => 'group', 'key' => 'description']);
+        $this->delete('setting', ['group' => 'group', 'key' => 'email']);
+        $this->delete('setting', ['group' => 'group', 'key' => 'dateformat']);
+        $this->delete('setting', ['group' => 'group', 'key' => 'timeformat']);
+        $this->delete('setting', ['group' => 'group', 'key' => 'timezone']);
+    }
+}
